f819c23a19401e843d83a410b8952c8f3b482f32
Petra Novakovic <pnovaznova@gmail.com>
2018-01-11 13:15:31 -0700

    added services

--- GuessTheNumber/Service1.svc.cs
+++ GuessTheNumber/Service1.svc.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -12,22 +13,55 @@ namespace GuessTheNumber
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+
+
+        public int secretNumber(int min, int max)
         {
-            return string.Format("You entered: {0}", value);
+
+            DateTime currentDate = DateTime.Now;
+
+            int seed = (int)currentDate.Ticks;
+
+            Random random = new Random(seed);
+
+            int sNumber = random.Next(min, max);
+
+            return sNumber;
+
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+
+        public string checkNumber(int userNum, int secretNum)
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            if (userNum == secretNum)
+
+                return "correct!";
+
+            else
+
+                if (userNum > secretNum)
+
+                return "too big";
+
+            else
+
+                if (userNum > secretNum)
+
+                return "too big!";
+
+            else
+
+                return "too small!";
+
+
+                  
+
+            
+ 
         }
     }
 }
+
+      
+    
+
---
+++ NumberApp/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NumberApp.numberServices;
+namespace NumberApp
+{
+    
+    
+    public partial class Form1 : Form
+    {
+        //global values that store the user's guess and actual secret number
+        int secretNumber;
+        int guess;
+        numberServices.Service1Client client = new numberServices.Service1Client();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int min = int.Parse(textBox1.Text);
+            int max = int.Parse(textBox2.Text);
+
+            if (textBox1.Text != "" && textBox2.Text != "") //make sure the user entered two numbers
+            {
+                secretNumber = client.secretNumber(min, max); //store secret number
+
+                label9.Text = "Secret number generated. Guess the number Below!";
+            }
+
+            else
+            {
+                label9.Text = "Please enter both min and max values to generate secret number.";
+            }
+
+          
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            guess = int.Parse(textBox3.Text); //value that the user guessed
+
+
+
+           label6.Text = client.checkNumber(guess, secretNumber); //get secret number and display as label, tells the user if they guessed correctly
+            
+           
+
+        
+
+
+        }
+    }
+}
